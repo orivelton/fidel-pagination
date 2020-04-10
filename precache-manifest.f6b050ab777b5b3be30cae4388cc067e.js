c1b4e5c01cbd34591ea3b020817b07495dfd7a91
Orivelton <orivelton@Oriveltons-MBP.Home>
2020-04-10 22:28:05 +0100

    Updates

---
+++ precache-manifest.f6b050ab777b5b3be30cae4388cc067e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411d704a9eefb21fccad09d5751bf6ce",
+    "url": "/fidel-pagination/index.html"
+  },
+  {
+    "revision": "982edadf101497f6e0d7",
+    "url": "/fidel-pagination/static/css/2.1b8f7479.chunk.css"
+  },
+  {
+    "revision": "fa4bf9a6948f18ad3822",
+    "url": "/fidel-pagination/static/css/main.abe4488e.chunk.css"
+  },
+  {
+    "revision": "982edadf101497f6e0d7",
+    "url": "/fidel-pagination/static/js/2.c27eb272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fidel-pagination/static/js/2.c27eb272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4bf9a6948f18ad3822",
+    "url": "/fidel-pagination/static/js/main.99884817.chunk.js"
+  },
+  {
+    "revision": "92376b9dfa864ca9ff20",
+    "url": "/fidel-pagination/static/js/runtime-main.400f864e.js"
+  }
+]);
